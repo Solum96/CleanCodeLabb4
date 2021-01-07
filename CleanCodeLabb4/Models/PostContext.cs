646d824d355f920f71325ec297225258752db8ee
Nils Lundell <Nils.Lundell@mardigras.se>
2021-01-07 11:20:23 +0100

    Added REST api with sqlite

---
+++ CleanCodeLabb4/Models/PostContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanCodeLabb4.Models
+{
+    public class PostContext : DbContext
+    {
+        public PostContext(DbContextOptions<PostContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=MyDatabase.db");
+        }
+    }
+}
